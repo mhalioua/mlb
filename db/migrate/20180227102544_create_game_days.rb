7e808dd8c65bb5c2e1090930ea836a72822d0b51
Apollo <super.hero2017@outlook.com>
2018-02-27 06:38:06 -0500

    Changed DB

--- db/migrate/20180227102544_create_game_days.rb
+++ db/migrate/20180227102544_create_game_days.rb
@@ -1,9 +1,7 @@
 class CreateGameDays < ActiveRecord::Migration[5.1]
   def change
     create_table :game_days do |t|
-      t.integer    :year
-      t.integer    :month
-      t.integer    :day
+      t.date    :date
       t.timestamps
     end
   end
