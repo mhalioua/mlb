e3cb5dcb0d5f5ea09cea60b63b565954d7e032eb
Apollo <super.hero2017@outlook.com>
2018-03-06 12:22:36 -0500

    Fix fangrpahi_id

--- db/migrate/20180303223416_create_players.rb
+++ db/migrate/20180303223416_create_players.rb
@@ -4,7 +4,7 @@ class CreatePlayers < ActiveRecord::Migration[5.1]
       t.belongs_to :team,  index: true
       t.string   :name
       t.string   :identity
-      t.integer  :fangraph_id
+      t.string  :fangraph_id
       t.string   :bathand
       t.string   :throwhand
       t.integer  :age
