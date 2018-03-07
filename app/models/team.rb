7233a6d25aa56b4ebfc00b2dfbff3d16de6a3188
Apollo <super.hero2017@outlook.com>
2018-03-07 05:25:34 -0500

    Fix

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   has_many :players
-  has_many :lancers
-  has_many :batters
+  has_many :lancers, dependent: :nullify
+  has_many :batters, dependent: :nullify
 
   def self.create_teams
     Create::Teams.create
