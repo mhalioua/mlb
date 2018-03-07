bdb054d3a9c2dd8cf8a688cb2b24fbeea9b16c28
Apollo <super.hero2017@outlook.com>
2018-03-07 05:02:39 -0500

    Fix

--- app/models/player.rb
+++ app/models/player.rb
@@ -26,7 +26,7 @@ class Player < ApplicationRecord
       end
     else
       unless batter = batters.find_by(season: season, team: nil, game: nil)
-        batter = batters.create(player_id: self.id, season_id: season.id)
+        batter = batters.create(player_id: self.id, season_id: season.id, team: nil, game: nil)
       end
     end
     return batter
@@ -41,7 +41,7 @@ class Player < ApplicationRecord
       end
     else
       unless lancer = lancers.find_by(season: season, team: nil, game: nil)
-        lancer = lancers.create(player_id: self.id, season_id: season.id)
+        lancer = lancers.create(player_id: self.id, season_id: season.id, team: nil, game: nil)
       end
     end
     return lancer
