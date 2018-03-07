f7ead47ffc408f9c593592c11adb70fac5159bf3
Apollo <super.hero2017@outlook.com>
2018-03-07 05:08:26 -0500

    Fix

--- db/migrate/20180303224527_create_batters.rb
+++ db/migrate/20180303224527_create_batters.rb
@@ -1,10 +1,10 @@
 class CreateBatters < ActiveRecord::Migration[5.1]
   def change
     create_table :batters do |t|
-      t.belongs_to :team,        index: true
-      t.belongs_to :game,        index: true
-      t.belongs_to :player,      index: true
-      t.belongs_to :season,      index: true
+      t.belongs_to :team
+      t.belongs_to :game
+      t.belongs_to :player
+      t.belongs_to :season
       t.boolean    :starter
       t.integer    :lineup
       t.string     :position
