a684742b848d1549471e9ea5b3c2bedb2996d2d1
Apollo <super.hero2017@outlook.com>
2018-03-07 04:59:06 -0500

    Fixing create batter and pitcher

--- app/models/player.rb
+++ app/models/player.rb
@@ -20,13 +20,13 @@ class Player < ApplicationRecord
   def create_batter(season, team=nil, game=nil)
     if game
       unless batter = batters.find_by(season: season, team: team, game: game)
-        batter = Batter.create(player: self, season: season, team: team, game: game)
+        batter = batters.create(season: season, team: team, game: game)
         puts "#{self.name} batter created for #{game.url}"
         batter.create_game_stats
       end
     else
       unless batter = batters.find_by(season: season, team: nil, game: nil)
-        batter = Batter.create(player_id: self.id, season_id: season.id)
+        batter = batters.create(season: season)
       end
     end
     return batter
@@ -35,13 +35,13 @@ class Player < ApplicationRecord
   def create_lancer(season, team=nil, game=nil)
     if game
       unless lancer = lancers.find_by(season: season, team_id: team, game: game)
-        lancer = Lancer.create(player: self, season: season, team: team, game: game)
+        lancer = lancers.create(season: season, team: team, game: game)
         puts "#{self.name} lancer created for #{game.url}"
         lancer.create_game_stats
       end
     else
       unless lancer = lancers.find_by(season: season, team: nil, game: nil)
-        lancer = Lancer.create(player_id: self.id, season_id: season.id)
+        lancer = lancers.create(season: season)
       end
     end
     return lancer
