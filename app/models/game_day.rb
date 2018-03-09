ea470777f40c64e9414e89f5b80d2bdf7ec704cd
Apollo <super.hero2017@outlook.com>
2018-03-09 17:26:43 -0500

    Fix gameday model

--- app/models/game_day.rb
+++ app/models/game_day.rb
@@ -3,7 +3,7 @@ class GameDay < ApplicationRecord
   has_many :games, dependent: :destroy
 
   def self.search(date)
-    return GameDay.find_or_create_by(date: date)
+    return GameDay.find_or_create_by(season: Season.find_by_year(date.year), date: date)
   end
 
   def year
