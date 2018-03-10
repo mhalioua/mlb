444887a81c1d9cb1f6dd3ef90d0093f7b21437da
Apollo <super.hero2017@outlook.com>
2018-03-10 03:29:13 -0500

    Change find_or_create_by

--- app/models/create/matchups.rb
+++ app/models/create/matchups.rb
@@ -58,7 +58,9 @@ module Create
           element = doc.css(".game-date-time").first
           game_date = element.children[1]['data-date']
           date = DateTime.parse(game_date) - 4.hours - home_team.timezone.hours
-          game = Game.find_or_create_by(game_id: game_id)
+          unless game = Game.find_by(game_id: game_id)
+            game = Game.create(game_days_id: game_day.id, away_team_id: away_team.id, home_team_id: home_team.id, game_id: game_id)
+          end
           game.update(game_days_id: game_day.id, away_team_id: away_team.id, home_team_id: home_team.id, game_date: date)
         end
       end
