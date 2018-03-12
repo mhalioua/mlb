0f18822b789bca66b71cd3618733ddb10def0b1f
Apollo <super.hero2017@outlook.com>
2018-03-12 18:45:50 -0400

    Change childs

--- app/models/update/games.rb
+++ app/models/update/games.rb
@@ -25,7 +25,7 @@ module Update
             break
           end
           if index%2 == 1
-            abbr = stat.child.text[0...-3].to_s
+            abbr = stat.text[0...-3].to_s
             abbr = 'CHW' if abbr == 'CWS'
             team = Team.find_by(espn_abbr: abbr)
             add_game_to_array(game_array, day_games, team)   
