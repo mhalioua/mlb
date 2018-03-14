f8c461c173126f7ec1e64409527bf53c9250cb02
Apollo <super.hero2017@outlook.com>
2018-03-14 15:58:39 +0000

    Add pages

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,5 +12,33 @@ class GameController < ApplicationController
 		month = Date::MONTHNAMES[@game_day.month]
 		day = @game_day.day.to_s
 		@date = "#{month} #{day}"
+
+		@away_starting_lancer = @game.lancers.where(team: @away_team, starter: true)
+		@home_starting_lancer = @game.lancers.where(team: @home_team, starter: true)
+
+		unless @away_starting_lancer.empty?
+			@home_left = @away_starting_lancer.first.throwhand == "L"
+			@home_batters = @away_starting_lancer.first.opposing_lineup
+			if @home_batters.empty?
+			  @home_predicted = "Predicted "
+			  @home_batters = @away_starting_lancer.first.predict_opposing_lineup
+			end
+		else
+			@home_batters = Batter.none
+		end
+
+		unless @home_starting_lancer.empty?
+			@away_left = @home_starting_lancer.first.throwhand == "L"
+			@away_batters = @home_starting_lancer.first.opposing_lineup
+			if @away_batters.empty?
+			  @away_predicted = "Predicted "
+			  @away_batters = @home_starting_lancer.first.predict_opposing_lineup
+			end
+		else
+			@away_batters = Batter.none
+		end
+
+		@away_bullpen_lancers = @game.lancers.where(team_id: @away_team.id, bullpen: true)
+		@home_bullpen_lancers = @game.lancers.where(team_id: @home_team.id, bullpen: true)
 	end
 end
