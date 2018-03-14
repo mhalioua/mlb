f44d4fe3ebb16b98f502208db64ede59aefc04dc
Apollo <super.hero2017@outlook.com>
2018-03-14 17:26:09 +0000

    Fix validation

--- app/models/batter_stat.rb
+++ app/models/batter_stat.rb
@@ -2,7 +2,11 @@ class BatterStat < ApplicationRecord
   belongs_to :batter
 
   def tld
-    tld = ((ld/100 * ab + bb) / (bb + ab) * 100).round(1)
-    tld.nan? ? 0 : tld
+    unless ld
+      0
+    else
+      tld = ((ld/100 * ab + bb) / (bb + ab) * 100).round(1)
+      tld.nan? ? 0 : tld
+    end
   end
 end
