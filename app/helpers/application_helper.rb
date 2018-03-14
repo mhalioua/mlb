a5475b233d4e90db3a5060a3017f524c06e17b1f
Apollo <super.hero2017@outlook.com>
2018-03-14 15:40:07 +0000

    Fix span

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def mixed_statistic(lefty_stat, righty_stat, num_lefty, num_righty)
 		if num_lefty + num_righty == 0
-			lefty_stat
+			0
 		elsif lefty_stat == nil || righty_stat == nil
 			0
 		else
