d382bfd53c63428a9e2b936d4e7b26fed9621b54
Apollo <super.hero2017@outlook.com>
2018-03-14 14:25:01 +0000

    Add validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
 	def mixed_statistic(lefty_stat, righty_stat, num_lefty, num_righty)
 		if num_lefty + num_righty == 0
 			lefty_stat
+		elsif lefty_stat == nil || righty_stat == nil
+			0
 		else
 			((lefty_stat * num_lefty + righty_stat * num_righty)/(num_lefty + num_righty)).round(2)
 		end
