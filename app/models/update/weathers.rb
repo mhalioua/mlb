9483695a23662767da83d664a480842b0eea28c1
Apollo <super.hero2017@outlook.com>
2018-03-17 09:06:24 -0400

    Fix

--- app/models/update/weathers.rb
+++ app/models/update/weathers.rb
@@ -6,7 +6,7 @@ module Update
     def update(game)
       game_day = game.game_day
       home_team = game.home_team
-      time = DateTime.parse(game.game_date).to_time
+      time = DateTime.parse(game.game_date).strftime("%I:%M%p").to_time
 
       url = get_url(home_team, game_day)
       doc = download_document(url)
