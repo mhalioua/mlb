33e2e40f64a22c1254f85e95f7cf1ad8cb137a0a
Apollo <super.hero2017@outlook.com>
2018-03-21 16:40:32 -0400

    Fix

--- db/migrate/20180321202721_create_workbooks.rb
+++ db/migrate/20180321202721_create_workbooks.rb
@@ -1,10 +1,13 @@
 class CreateWorkbooks < ActiveRecord::Migration[5.1]
   def change
     create_table :workbooks do |t|
+      t.string :Home_Team
       t.float :TEMP
       t.float :DP
       t.float :HUMID
       t.float :BARo
+      t.string :M
+      t.float :N
       t.float :R
       t.float :Total_Hits
       t.float :Total_Walks
