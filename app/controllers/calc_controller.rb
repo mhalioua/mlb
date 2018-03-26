00b4718c0451b325443bd4bee6a597535b86b515
Apollo <super.hero2017@outlook.com>
2018-03-26 12:40:16 -0400

    Mlb calculator

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,24 @@
+class CalcController < ApplicationController
+  require 'date'
+
+  def input
+    @post = params
+    @teams = Team.all
+    @team_dropdown = []
+    @teams.each do |team|
+      team_element = [team.name, team.name]
+      @team_dropdown << team_element
+    end
+    @team_dropdown = @team_dropdown.sort
+    if @post['authenticity_token']
+      if @post['form_stadium']
+        element = @teams.find{|x| x.name == @post['form_stadium'] }
+        zipcode = element.zipcode
+        if element.id == 4
+          zipcode = "M5V1J1"
+        end
+        @stadium = stadium_weather(zipcode)
+      end
+    end
+  end
+end
---
+++ app/helpers/calc_helper.rb
@@ -0,0 +1,21 @@
+module CalcHelper
+    def stadium_weather(zipcode)
+    require 'open-uri'
+    require 'json'
+
+    url = "http://api.wunderground.com/api/65bd4b6d02af0c3b/hourly/q/#{zipcode}.json"
+    re = []
+    open(url) do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      forecast_data = parsed_json['hourly_forecast']
+      (0..2).each do |i|
+        hour_data = forecast_data[i]
+        data = {temp: hour_data['temp']['english'], humidity: hour_data['humidity'], dew: hour_data['dewpoint']['english'], pressure: hour_data['mslp']['english'], wind_dir: hour_data['wdir']['dir'], wind_speed: hour_data['wspd']['english'].to_f}
+        re << data
+      end
+    end
+
+    return re
+  end
+end
