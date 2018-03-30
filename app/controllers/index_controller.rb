f3b4d523013a81e3d0e817bb7e6a03617ef9a74c
Apollo <super.hero2017@outlook.com>
2018-03-30 19:38:29 -0400

    Test

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,6 +10,6 @@ class IndexController < ApplicationController
 	def game
 		game_day = GameDay.find(params[:id])
 		@head = "#{Date::MONTHNAMES[game_day.month]} #{game_day.day.ordinalize}"
-		@games = game_day.games.order(:created_at)
+		@games = game_day.games.sort_by{|game| (DateTime.parse(game.game_date) - game.home_team.timezone.hours) }
 	end
 end
