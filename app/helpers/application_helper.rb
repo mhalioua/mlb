5e7daddb9f463885c66057cd93e80d2303bad31a
Apollo <super.hero2017@outlook.com>
2018-04-22 18:00:16 -0400

    FIx

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
 			((lefty_stat * num_lefty + righty_stat * num_righty)/(num_lefty + num_righty)).round(2)
 		end
 	end
+	def mixed_statistic_ip(lefty_stat, righty_stat, num_lefty, num_righty)
+		if num_lefty + num_righty == 0
+			0
+		elsif lefty_stat == nil || righty_stat == nil
+			0
+		else
+			decimal = ((lefty_stat - lefty_stat.to_i)*10).round() + ((righty_stat - righty_stat.to_i)* 10).round()
+			sum = lefty_stat.to_i * num_lefty + righty_stat.to_i * num_righty
+			sum + (decimal/3).to_i + (decimal%3)/10
+		end
+	end
 end
