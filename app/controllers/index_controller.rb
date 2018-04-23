6c96653bfe28c3a324534dc2dd2b02e1b02f2a7c
Apollo <super.hero2017@outlook.com>
2018-04-23 03:14:07 -0400

    Fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -18,6 +18,6 @@ class IndexController < ApplicationController
 	  	@gameDays = GameDay.where("date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
 	  	game_day = Date.strptime(@game_start_index, '%Y-%m-%d')
 		@head = "#{game_day.strftime("%B")} #{game_day.strftime("%e").to_i.ordinalize}"
-		@games = game_day.games.sort_by{|game| (DateTime.parse(game.game_date) - game.home_team.timezone.hours) }
+		@games = @gameDays.games.sort_by{|game| (DateTime.parse(game.game_date) - game.home_team.timezone.hours) }
 	end
 end
