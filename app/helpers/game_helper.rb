09346b6ee36c7446e06ca6a9c107886ea45f5d1d
Apollo <super.hero2017@outlook.com>
2018-05-22 06:12:03 -0400

    Fix

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -527,6 +527,9 @@ module GameHelper
     search_string_dup = search_string.dup
     search_string_low_dup = search_string_low.dup
 
+    wind_dir1 = filter_wind(wind_dir1)
+    wind_dir2 = filter_wind(wind_dir2)
+
     if name != ""
       search_string.push('"M" IN ' + "('#{wind_dir1}', '#{wind_dir2}')")
       search_string_low.push('"M" IN ' + "('#{wind_dir1}', '#{wind_dir2}')")
@@ -661,6 +664,20 @@ module GameHelper
     return result
   end
 
+  def filter_wind(wind)
+    if wind == 'W'
+      'West'
+    elsif wind == 'E'
+      'East'
+    elsif wind == 'N'
+      'North'
+    elsif wind == 'S'
+      'South'
+    else
+      wind
+    end
+  end
+
   def true_wind_prev(temp_min, temp_max, dew_min, dew_max, humid_min, humid_max, baro_min, baro_max, wind_min, wind_max, wind_dir1, wind_dir2, name)
     search_string = []
     search_string_low = []
@@ -701,6 +718,9 @@ module GameHelper
 
     search_string_dup = search_string.dup
     search_string_low_dup = search_string_low.dup
+
+    wind_dir1 = filter_wind(wind_dir1)
+    wind_dir2 = filter_wind(wind_dir2)
 
     if name != ""
       search_string.push('"M" IN ' + "('#{wind_dir1}', '#{wind_dir2}')")
