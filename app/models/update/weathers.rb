8bbe192949d58c8ac58498d1f13f552607b542b5
Apollo <super.hero2017@outlook.com>
2018-06-11 18:46:15 +0800

    FIx

--- app/models/update/weathers.rb
+++ app/models/update/weathers.rb
@@ -67,6 +67,7 @@ module Update
       weathers = game.weathers.where(station: "Actual").order(:hour)
       row_number = 0
       block_number = 0
+      return unless weathers.length
       date = weathers.first.updated_at.advance(hours: game.home_team.timezone).in_time_zone('Eastern Time (US & Canada)').strftime("%F %I:%M%p")
 
       Weathersource.where(game_id: game.id, date: date, table_number: 2).destroy_all
