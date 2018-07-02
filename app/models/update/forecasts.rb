3a4abf4fc1bd9cfb5f9be3f585296bb4b36ab667
Apollo <super.hero2017@outlook.com>
2018-07-02 19:52:01 -0400

    FIx

--- app/models/update/forecasts.rb
+++ app/models/update/forecasts.rb
@@ -41,10 +41,10 @@ module Update
       return if start_index < 0 || hourlyweathers[0].children[2].text.squish.to_time > time
       hourlyweathers.each_with_index do |weather, index|
         date = weather.children[2].text.squish.to_time
+        start_index = index
         if date >= time
           break
         end
-        start_index = index
       end
 
       (1..4).each do |index|
