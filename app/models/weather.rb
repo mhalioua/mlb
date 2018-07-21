a5029e379a57bb026ba650c9ae0d48e31318eded
Apollo <super.hero2017@outlook.com>
2018-07-21 17:42:46 +0200

    Change Weather Number

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,22 +3,22 @@ class Weather < ApplicationRecord
 
   def temp_num
     return 0.0 if temp.size < 3
-    temp[0..-3].to_f.round(1)
+    temp.to_f.round(1)
   end
 
   def dew_num
     return 0.0 if dp.size < 3
-    dp[0..-3].to_f.round(1)
+    dp.to_f.round(1)
   end
 
   def humid_num
     return 0.0 if hum.size < 2
-    hum[0..1].to_i
+    hum.to_i
   end
 
   def pressure_num
     return 0.0 if pressure.size < 3
-    pressure[0..-3].to_f.round(2)
+    pressure.to_f.round(2)
   end
 
   def wind
