472a78ffa0bc770010c0dfdaedebe918c6170621
superhero2017 <super.hero2017@outlook.com>
2018-08-14 12:14:33 +0200

    Add view

--- db/migrate/20180814100446_add_closer_to_game.rb
+++ db/migrate/20180814100446_add_closer_to_game.rb
@@ -1,8 +1,8 @@
 class AddCloserToGame < ActiveRecord::Migration[5.1]
   def change
-    add_column :game, :away_money_line_closer, :string
-    add_column :game, :home_money_line_closer, :string
-    add_column :game, :away_total_closer, :string
-    add_column :game, :home_total_closer, :string
+    add_column :games, :away_money_line_closer, :string
+    add_column :games, :home_money_line_closer, :string
+    add_column :games, :away_total_closer, :string
+    add_column :games, :home_total_closer, :string
   end
 end
