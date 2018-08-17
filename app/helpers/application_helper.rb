e712b59e4a70bb5b43322c16f45dbfd470b20b5f
superhero2017 <super.hero2017@outlook.com>
2018-08-17 11:58:49 +0200

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,4 @@ module ApplicationHelper
 			((lefty_stat * num_lefty + righty_stat * num_righty)/(num_lefty + num_righty)).round(2)
 		end
 	end
-	def mixed_statistic_ip(lefty_stat, righty_stat)
-		decimal = ((lefty_stat - lefty_stat.to_i)*10).round() + ((righty_stat - righty_stat.to_i)* 10).round()
-		sum = lefty_stat.to_i + righty_stat.to_i
-		sum = sum * 3 + decimal
-		return "#{(sum/3).to_i}.#{(sum%3).to_i}"
-	end
 end
--- app/models/update/pitchers.rb
+++ app/models/update/pitchers.rb
@@ -607,5 +607,22 @@ module Update
       return handedness
     end
 
+    def mixed_statistic_ip(lefty_stat, righty_stat)
+      decimal = ((lefty_stat - lefty_stat.to_i)*10).round() + ((righty_stat - righty_stat.to_i)* 10).round()
+      sum = lefty_stat.to_i + righty_stat.to_i
+      sum = sum * 3 + decimal
+      return "#{(sum/3).to_i}.#{(sum%3).to_i}"
+    end
+
+
+    def mixed_statistic(lefty_stat, righty_stat, num_lefty, num_righty)
+      if num_lefty + num_righty == 0
+        0
+      elsif lefty_stat == nil || righty_stat == nil
+        0
+      else
+        ((lefty_stat * num_lefty + righty_stat * num_righty)/(num_lefty + num_righty)).round(2)
+      end
+    end
   end
 end
