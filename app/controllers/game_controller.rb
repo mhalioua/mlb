a10edbdc44f2d3d98f1fc05029a5d6a5e3f4ba64
superhero2017 <super.hero2017@outlook.com>
2018-08-23 14:19:05 +0200

    Fix

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -72,6 +72,8 @@ class GameController < ApplicationController
 
 		@away_hitters = @game.hitters.where(team_id: @away_team.id).order(:index)
 		@home_hitters = @game.hitters.where(team_id: @home_team.id).order(:index)
+
+		@playbyplay = @game.playbyplay.first
 	end
 
 	def weather
@@ -156,6 +158,8 @@ class GameController < ApplicationController
 
 		@away_hitters = @game.hitters.where(team_id: @away_team.id).order(:index)
 		@home_hitters = @game.hitters.where(team_id: @home_team.id).order(:index)
+
+		@playbyplay = @game.playbyplay.first
 	end
 
 	def previous
