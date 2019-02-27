2dafee6b7dc1c395063e08af7bb5a4a941aa89b5
Apollo <super.hero2017@outlook.com>
2019-02-27 23:25:53 +0800

    Fix

--- app/models/update/games.rb
+++ app/models/update/games.rb
@@ -28,7 +28,7 @@ module Update
           break
         end
         if index % 2 == 1
-          abbr = stat.text[0...-3].to_s
+          abbr = stat.child.text.to_s
           abbr = fix_abbr(abbr)
           team = Team.find_by(espn_abbr: abbr)
           add_game_to_array(game_array, day_games, team)
