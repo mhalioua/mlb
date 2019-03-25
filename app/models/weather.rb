9bd81bde3b121f531fb53d773dd6a323cdc232a6
Apollo <super.hero2017@outlook.com>
2019-03-25 13:43:24 +0100

    Fix

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,22 +2,18 @@ class Weather < ApplicationRecord
   belongs_to :game
 
   def temp_num
-    return 0.0 if temp.size < 3
     temp.to_f.round(1)
   end
 
   def dew_num
-    return 0.0 if dp.size < 3
     dp.to_f.round(1)
   end
 
   def humid_num
-    return 0.0 if hum.size < 2
     hum.to_i
   end
 
   def pressure_num
-    return 0.0 if pressure.size < 3
     pressure.to_f.round(2)
   end
 
