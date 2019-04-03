2bd7716debd7b6733f5bfdb6fef17f4983dc9da1
Apollo <super.hero2017@outlook.com>
2019-04-03 09:37:46 +0200

    Fix

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -49,6 +49,8 @@ class GameController < ApplicationController
 
 		@forecast_dropdown = []
 		@forecasts.each_with_index do |forecast_one, index|
+			next if index % 2 == 0
+			break if index == 11
       if index != 0
 			  @forecast_dropdown << [forecast_one.updated_at.advance(hours: @home_team.timezone).in_time_zone('Eastern Time (US & Canada)').strftime("%F %I:%M%p"), index]
       end
@@ -100,6 +102,8 @@ class GameController < ApplicationController
 
 		@forecast_dropdown = []
 		@forecasts.each_with_index do |forecast_one, index|
+			next if index % 2 == 0
+			break if index == 11
       if index != 0
 			  @forecast_dropdown << [forecast_one.updated_at.advance(hours: @home_team.timezone).in_time_zone('Eastern Time (US & Canada)').strftime("%F %I:%M%p"), index]
       end
