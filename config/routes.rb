f51e892a1703e6fc963dcf8cfa119607346aeec9
Apollo <super.hero2017@outlook.com>
2019-05-14 18:31:29 -0400

    Url Test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'index#home'
   get "game/new/:id/:forecast/:option", to: "game#new"
   get "game/weather/:id/:forecast/:option", to: "game#weather"
+  get "screen/new/:id/:forecast/:option", to: "screen#new"
+  get "screen/weather/:id/:forecast/:option", to: "screen#weather"
   match ':controller(/:action(/:id))', :via => [:get, :post]
 end
