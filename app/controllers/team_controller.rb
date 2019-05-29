a610221bfb9191522f5a65b2318b48e937ab5525
Apollo <super.hero2017@outlook.com>
2019-05-29 08:02:25 -0400

    Fix

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -6,6 +6,6 @@ class TeamController < ApplicationController
   def show
     id = params[:id]
     @team = Team.find_by(id: id)
-    @games = Game.where("home_team = ? AND game_date < ?", id, Date.today).or(Game.where("away_team = ? AND game_date < ?", id, Date.today)).order('game_date DESC').limit(50)
+    @games = Game.where("home_team = ? AND game_date < ?", id, DateTime.today).or(Game.where("away_team = ? AND game_date < ?", id, DateTime.today)).order('game_date DESC').limit(50)
   end
 end
