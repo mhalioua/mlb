eae73002746cbda1d38499aee9890aa6c44ec2f7
Apollo <super.hero2017@outlook.com>
2019-06-05 06:02:47 -0400

    Update teams

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -6,6 +6,6 @@ class TeamController < ApplicationController
   def show
     id = params[:id]
     @team = Team.find_by(id: id)
-    @games = Game.where("home_team_id = ? AND game_date < ?", id, Date.current).order('game_date DESC').limit(50)
+    @games = Game.where("home_team_id = ? AND game_date < ? AND id > 10516", id, Date.current).or(Game.where("home_team_id = ? AND game_date < ? AND id < 10070", id, Date.current)).order('game_date DESC').limit(50)
   end
 end
