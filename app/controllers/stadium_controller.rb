5da259155e15c2a9699c98fe2e2567be4c1394f7
Apollo <super.hero2017@outlook.com>
2019-07-15 06:09:50 -0400

    Test

---
+++ app/controllers/stadium_controller.rb
@@ -0,0 +1,10 @@
+class StadiumController < ApplicationController
+  def index
+    @teams = Team.all.order('name')
+  end
+
+  def show
+    id = params[:id]
+    @team = Team.find_by(id: id)
+  end
+end
