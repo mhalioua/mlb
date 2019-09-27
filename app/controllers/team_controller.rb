aaf779fb2700230efc315eec357950568f48f101
Apollo <super.hero2017@outlook.com>
2019-09-27 03:49:05 -0400

    Test

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -11,14 +11,14 @@ class TeamController < ApplicationController
 
   def filter
     unless params[:date]
-      params[:date] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
+      params[:date] = Time.now.strftime("%b %d, %Y") + " - " + Time.now.strftime("%b %d, %Y")
     end
     @game_index = params[:date]
     @game_start_index = @game_index[0..13]
     @game_end_index = @game_index[17..30]
     @teams = Team.all.order('name')
-    @result = Game.where("game_date < ? AND id > 10654 AND game_date between ? and ?", Date.current, Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
-                  .or(Game.where("game_date < ? AND id < 10070 AND id >= 9058 AND game_date between ? and ?", Date.current, Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)).order('game_date DESC')
+    @result = Game.where("game_date < ? AND id > 10654 AND game_date between ? and ?", Date.current, Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)
+                  .or(Game.where("game_date < ? AND id < 10070 AND id >= 9058 AND game_date between ? and ?", Date.current, Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)).order('game_date DESC')
     @team_id = nil
     @wind_dir = nil
     @wind_speed = nil
