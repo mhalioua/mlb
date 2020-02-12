c793d11deb12b1f16076cd3072e49033d74f389e
hammad417 <hammad417@gmail.com>
2020-02-12 14:46:46 +0500

    indices added in migration

--- db/migrate/20200212075914_add_indices.rb
+++ db/migrate/20200212075914_add_indices.rb
@@ -1,26 +1,23 @@
 class AddIndices < ActiveRecord::Migration[5.1]
   def change
-  	#add_index :transactions, :team_id
-  	#add_index :weather_firsts, :game_id
-  	#add_index :newworkbooks, :game_id
-  	
-  	#add_index :stadiumdatums, :team_id
-  	#add_index :stadiumdatums, :wind_dir
-  	#add_index :stadiums, :stadium
+=begin
+  	add_index :transactions, :team_id
+  	add_index :weather_firsts, :game_id
+  	add_index :newworkbooks, :game_id
 
-  	#add_index :batters, :starter
-  	#add_index :lancers, :ip
-  	#add_index :seasons, :year
-  	#add_index :pitcherinformations, :away
-  	#add_index :games, :game_id
+  	add_index :batters, :starter
+  	add_index :lancers, :ip
+  	add_index :seasons, :year
+  	add_index :pitcherinformations, :away
+  	add_index :games, :game_id
   	
-  	#add_index :newworkbooks, :Away_Team
-  	#add_index :newworkbooks, :hits1
-  	#add_index :newworkbooks, :link
-  	#add_index :newworkbooks, :Date
-  	#add_index :newworkbooks, :Time
-  	#add_index :workbooks, :hits1
-  	#add_index :workbooks, :t_HRS
+  	add_index :newworkbooks, :Away_Team
+  	add_index :newworkbooks, :hits1
+  	add_index :newworkbooks, :link
+  	add_index :newworkbooks, :Date
+  	add_index :newworkbooks, :Time
+  	add_index :workbooks, :hits1
+  	add_index :workbooks, :t_HRS
   	
   	add_index :results, :month
   	add_index :results, :year
@@ -39,5 +36,9 @@ class AddIndices < ActiveRecord::Migration[5.1]
   	add_index :results, :game_date
   	add_index :totals, :DATE
   	add_index :totals, :AWAY
+=end
+  	add_index :stadium_data, :team_id
+  	add_index :stadium_data, :wind_dir
+  	add_index :stadia, :stadium
   end
 end
