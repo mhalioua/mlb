839df99944308299c32bb314472c3dfa79215c05
apollo <super.hero2007@caramail.com>
2020-06-30 17:52:16 +0200

    feat: updated filter

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,5 @@
 class GameController < ApplicationController
-	before_action :confirm_logged_in
+	before_action :confirm_logged_in, except: ["new"]
 
 	def new
 		@game = Game.find_by_id(params[:id])
