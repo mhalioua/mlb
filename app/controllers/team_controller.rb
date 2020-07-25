3632a0d7198a1dd4816eeb7c3a4868a9e6f4263e
apollo <super.hero2007@caramail.com>
2020-07-25 18:18:29 +0200

    fix: fixed weather

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -113,7 +113,7 @@ class TeamController < ApplicationController
         end
 
         @games.push(game)
-        # break if @games.length === 50
+        break if @games.length === 50
       end
     end
   end
