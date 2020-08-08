90e27739df012523e8ffb14badd29ef845e4de5a
apollo <super.hero2007@caramail.com>
2020-08-08 18:20:33 +0200

    fix: fixed postpone

--- db/migrate/20200730072729_add_postpone_to_game.rb
+++ db/migrate/20200730072729_add_postpone_to_game.rb
@@ -1,5 +1,5 @@
 class AddPostponeToGame < ActiveRecord::Migration[5.1]
   def change
-    add_column :games, :postpone, :boolean
+    add_column :games, :postpone, :boolean, default: false
   end
 end
