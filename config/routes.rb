877453fec2d8caa5136b8faa5baeaddf98debfd5
Apollo <apollo@Apollos-iMac.local>
2020-08-27 21:27:23 +0200

    add event

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'index#home'
   get "game/new/:id/:forecast/:option(/:offset)", to: "game#new"
   get "game/weather/:id/:forecast/:option(/:offset)", to: "game#weather"
-  get "screen/new/:id/:forecast/:option(/:offset)", to: "screen#new"
-  get "screen/weather/:id/:forecast/:option(/:offset)", to: "screen#weather"
   match ':controller(/:action(/:id))', :via => [:get, :post]
 end
